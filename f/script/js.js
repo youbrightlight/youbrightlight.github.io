f23c24b4a2385915d1483e8dfc91561d8a483b9c
peu0em <peu0em@gmail.com>
2026-06-26 02:01:33 +0900

    animation

--- f/script/js.js
+++ f/script/js.js
@@ -10,7 +10,7 @@ windowtag.innerHTML = `
 const window_bar = document.createElement("div");
 window_bar.classList.add("bar","grab");
 window_bar.innerHTML = `
-  <img class='x' src='/f/visual/button_x.svg' alt='close' role='button'>
+  <img class='x click' src='/f/visual/button_x.svg' alt='close' role='button'>
 `
 const window_resizer = document.createElement("div");
 window_resizer.classList.add("resizer");
@@ -44,98 +44,156 @@ window_resizer.innerHTML = `
     {
       const windowSizeMin = 15 * rem;
 
-      //drag
-      { 
-        let dragTag;
-        let dragStartX, dragStartY, dragStartWidth, dragStartHeight, dragStartTop, dragStartLeft;
-        let osWidth, osHeight;
-        let dragXPaddingW, dragYPaddingN, dragXPaddingE, dragYPaddingS;
-        let dragMode = 0; // 1 - move, 2 - resize
-        let dragResizeDirection;
-        let dragDirectionN, dragDirectionS, dragDirectionW, dragDirectionE;
-        os.addEventListener("mousedown",(event)=>{
-          if(event.target.classList.contains("grab")){
-            event.preventDefault();
+      let targetTag;
+      let osWidth, osHeight;
+
+      let mode = [];
+      // [0] 1 = click, 2 = drag
+      // [1] click( 4 = close )
+      // [1] drag( 1 = move, 2 = resize )
+
+      let clickStartTag;
+
+      let dragStartX, dragStartY, dragStartWidth, dragStartHeight, dragStartTop, dragStartLeft;
+      let dragXPaddingW, dragYPaddingN, dragXPaddingE, dragYPaddingS;
+      let dragResizeDirection;
+      let dragDirectionN, dragDirectionS, dragDirectionW, dragDirectionE;
+
+      function tagRemove(tag){
+        const css = window.getComputedStyle(tag);
+        if(css.animationDuration !== "0s" && css.transitionDuration !== "0s"){
+          tag.addEventListener("animationed",()=> tag.remove(), { once: true });
+          tag.addEventListener("transitioned",()=> tag.remove(), { once: true });
+        }else{
+          setTimeout(()=>{
+            if(tag) tag.remove();
+          }, 3000);
+        }
+      } 
+
+      os.addEventListener("mousedown",(event)=>{
+        const classList = event.target.classList;
+        if(event.button === 0){
+          if(classList.contains("click")){
+            mode[0] = 1;
+            if(classList.contains("x")){
+              mode[1] = 4;
+              targetTag = event.target.parentNode.parentNode;
+            }
+          }else if(classList.contains("grab")){
+            mode[0] = 2;
             if(event.target.parentNode.classList.contains("resizer")){
-              dragMode = 2;
-              dragTag = event.target.parentNode.parentNode;
+              mode[1] = 2;
+              targetTag = event.target.parentNode.parentNode;
               dragResizeDirection = event.target.classList[0];
             }else if(event.target.classList.contains("bar")){
-              dragMode = 1;
-              dragTag = event.target.parentNode;
+              mode[1] = 1;
+              targetTag = event.target.parentNode;
             }
-            if(dragTag){
-              dragStartX = event.clientX;
-              dragStartY = event.clientY;
-              dragStartTop = dragTag.offsetTop;
-              dragStartLeft = dragTag.offsetLeft;
-              dragStartWidth = dragTag.offsetWidth;
-              dragStartHeight = dragTag.offsetHeight;
-              osWidth = os.clientWidth;
-              osHeight = os.clientHeight;
+          }
+          if((mode[0]>0) && targetTag){
+            event.preventDefault();
+            switch(mode[0]){
+              case 1:{
+                clickStartTag = event.target;
+                clickStartTag.classList.add("pressed");
+              }break;
+              case 2:{
+                dragStartX = event.clientX;
+                dragStartY = event.clientY;
+                dragStartTop = targetTag.offsetTop;
+                dragStartLeft = targetTag.offsetLeft;
+                dragStartWidth = targetTag.offsetWidth;
+                dragStartHeight = targetTag.offsetHeight;
+                osWidth = os.clientWidth;
+                osHeight = os.clientHeight;
 
-              switch(dragMode){
-                case 1:{
-                  dragXPaddingE = osWidth - dragStartWidth - osPadding;
-                  dragYPaddingS = osHeight - dragStartHeight - osPadding;
-                } break;
-                case 2:{
-                  dragDirectionN = false, dragDirectionS = false, dragDirectionW = false, dragDirectionE = false;
-                  switch(dragResizeDirection){
-                    case "nw": dragDirectionN = true; dragDirectionW = true; break;
-                    case "ne": dragDirectionN = true; dragDirectionE = true; break;
-                    case "sw": dragDirectionS = true; dragDirectionW = true; break;
-                    case "se": dragDirectionS = true; dragDirectionE = true; break;
-                    case "n": dragDirectionN = true; break;
-                    case "s": dragDirectionS = true; break;
-                    case "w": dragDirectionW = true; break;
-                    case "e": dragDirectionE = true; break;
-                  }
-                  dragXPaddingW = 0 - dragStartLeft + osPadding;
-                  dragXPaddingE = osWidth - (dragStartLeft + dragStartWidth) - osPadding;
-                  dragYPaddingN = 0 - dragStartTop + osPadding;
-                  dragYPaddingS = osHeight - (dragStartTop + dragStartHeight) - osPadding;
-                  dragXMin = dragStartWidth - windowSizeMin;
-                  dragYMin = dragStartHeight - windowSizeMin;
-                } break;
+                switch(mode[1]){
+                  case 1:{
+                    dragXPaddingE = osWidth - dragStartWidth - osPadding;
+                    dragYPaddingS = osHeight - dragStartHeight - osPadding;
+                  }break;
+                  case 2:{
+                    dragDirectionN = false, dragDirectionS = false, dragDirectionW = false, dragDirectionE = false;
+                    switch(dragResizeDirection){
+                      case "nw": dragDirectionN = true; dragDirectionW = true; break;
+                      case "ne": dragDirectionN = true; dragDirectionE = true; break;
+                      case "sw": dragDirectionS = true; dragDirectionW = true; break;
+                      case "se": dragDirectionS = true; dragDirectionE = true; break;
+                      case "n": dragDirectionN = true; break;
+                      case "s": dragDirectionS = true; break;
+                      case "w": dragDirectionW = true; break;
+                      case "e": dragDirectionE = true; break;
+                    }
+                    dragXPaddingW = 0 - dragStartLeft + osPadding;
+                    dragXPaddingE = osWidth - (dragStartLeft + dragStartWidth) - osPadding;
+                    dragYPaddingN = 0 - dragStartTop + osPadding;
+                    dragYPaddingS = osHeight - (dragStartTop + dragStartHeight) - osPadding;
+                    const css = window.getComputedStyle(targetTag);
+                    dragXMin = dragStartWidth - (css.minWidth?parseFloat(css.minWidth):windowSizeMin);
+                    dragYMin = dragStartHeight - (css.minHeight?parseFloat(css.minHeight):windowSizeMin);
+                  }break;
+                }
+              }break;
+            }
+          }
+        }
+      });
+      os.addEventListener("mousemove",(event)=>{
+        if(targetTag){
+          const dragXMove = event.clientX - dragStartX;
+          const dragYMove = event.clientY - dragStartY;
+          switch(mode[1]){
+            case 1:{
+              let x = Math.max(osPadding, Math.min(dragXMove + dragStartLeft, dragXPaddingE));
+              let y = Math.max(osPadding, Math.min(dragYMove + dragStartTop, dragYPaddingS));
+              targetTag.style.left = x + "px";
+              targetTag.style.top = y + "px";
+            }break;
+            case 2:{
+              if(dragDirectionN){
+                let y = (dragYMove < 0) ? (Math.max(dragYMove, dragYPaddingN)) : (Math.min(dragYMove, dragYMin));
+                targetTag.style.height = dragStartHeight - y + "px";
+                targetTag.style.top = dragStartTop + y + "px";
+              }else if(dragDirectionS){
+                let y = Math.min(dragYMove, dragYPaddingS);
+                targetTag.style.height = dragStartHeight + y + "px";
+              }
+              if(dragDirectionW){
+                let x = (dragXMove < 0) ? (Math.max(dragXMove, dragXPaddingW)) : (Math.min(dragXMove, dragXMin));
+                targetTag.style.width = dragStartWidth - x + "px";
+                targetTag.style.left = dragStartLeft + x  + "px";
+              }else if(dragDirectionE){
+                let x = Math.min(dragXMove, dragXPaddingE);
+                targetTag.style.width = dragStartWidth + x + "px";
+              }
+            }break;
+          }
+        }
+      });
+      os.addEventListener("mouseup",(event)=>{
+        switch(mode[0]){
+          case 1:{
+            clickStartTag.classList.remove("pressed");
+            if(event.target == clickStartTag){
+              switch(mode[1]){
+                case 4:{
+                  targetTag.classList.add("close");
+                  tagRemove(targetTag);
+                }
               }
             }
-          }
-        });
-        os.addEventListener("mousemove",(event)=>{
-          if(dragTag){
-            const dragXMove = event.clientX - dragStartX;
-            const dragYMove = event.clientY - dragStartY;
-            switch(dragMode){
-              case 1:{
-                let x = Math.max(osPadding, Math.min(dragXMove + dragStartLeft, dragXPaddingE));
-                let y = Math.max(osPadding, Math.min(dragYMove + dragStartTop, dragYPaddingS));
-                dragTag.style.left = x + "px";
-                dragTag.style.top = y + "px";
-              } break;
-              case 2:{
-                if(dragDirectionN){
-                  let y = (dragYMove < 0) ? (Math.max(dragYMove, dragYPaddingN)) : (Math.min(dragYMove, dragYMin));
-                  dragTag.style.height = dragStartHeight - y + "px";
-                  dragTag.style.top = dragStartTop + y + "px";
-                }else if(dragDirectionS){
-                  let y = Math.min(dragYMove, dragYPaddingS);
-                  dragTag.style.height = dragStartHeight + y + "px";
-                }
-                if(dragDirectionW){
-                  let x = (dragXMove < 0) ? (Math.max(dragXMove, dragXPaddingW)) : (Math.min(dragXMove, dragXMin));
-                  dragTag.style.width = dragStartWidth - x + "px";
-                  dragTag.style.left = dragStartLeft + x  + "px";
-                }else if(dragDirectionE){
-                  let x = Math.min(dragXMove, dragXPaddingE);
-                  dragTag.style.width = dragStartWidth + x + "px";
-                }
-              } break;
-            }
-          }
-        });
-        os.addEventListener("mouseup",()=>{ dragTag = null; });
-      }
+          }break;
+        }
+        targetTag = null;
+      });
+      os.addEventListener("mouseleave",()=>{
+        switch(mode[0]){
+          case 1:{
+            targetTag = null;
+          }break;
+        }
+      });
     }
   }
 
