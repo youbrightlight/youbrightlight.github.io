d5c6dae39d1e14fbdc88555873127daa57da60e5
peu0em <peu0em@gmail.com>
2026-06-25 22:38:40 +0900

    window transform

--- f/script/js.js
+++ f/script/js.js
@@ -1,3 +1,7 @@
+const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+const osPadding = 1 * rem;
+
 const windowtag = document.createElement("div");
 windowtag.classList.add("windowtag");
 windowtag.innerHTML = `
@@ -24,6 +28,7 @@ window_resizer.innerHTML = `
 
 {
   function afterLoad(){
+    const os = document.getElementById("os");
 
 
     const windowFirst = document.getElementsByClassName("window");
@@ -35,83 +40,102 @@ window_resizer.innerHTML = `
     }
 
 
-
+    // window transform 
     {
-      const dragContainer = document.body;
-      let dragTag;
-      let dragStartX, dragStartY, dragStartWidth, dragStartHeight, dragStartTop, dragStartLeft;
-      let xMax, yMax;
-      let dragMode = 0; // 1 - move, 2 - resize
-      let dragResizeDirection;
-      dragContainer.addEventListener("mousedown",(event)=>{
-        if(event.target.classList.contains("grab")){
-          event.preventDefault();
-          if(event.target.parentNode.classList.contains("resizer")){
-            dragMode = 2;
-            dragTag = event.target.parentNode.parentNode;
-            dragResizeDirection = event.target.classList[0];
-          }else if(event.target.classList.contains("bar")){
-            dragMode = 1;
-            dragTag = event.target.parentNode;
+      const windowSizeMin = 15 * rem;
+
+      //drag
+      { 
+        let dragTag;
+        let dragStartX, dragStartY, dragStartWidth, dragStartHeight, dragStartTop, dragStartLeft;
+        let osWidth, osHeight;
+        let dragXPaddingW, dragYPaddingN, dragXPaddingE, dragYPaddingS;
+        let dragMode = 0; // 1 - move, 2 - resize
+        let dragResizeDirection;
+        let dragDirectionN, dragDirectionS, dragDirectionW, dragDirectionE;
+        os.addEventListener("mousedown",(event)=>{
+          if(event.target.classList.contains("grab")){
+            event.preventDefault();
+            if(event.target.parentNode.classList.contains("resizer")){
+              dragMode = 2;
+              dragTag = event.target.parentNode.parentNode;
+              dragResizeDirection = event.target.classList[0];
+            }else if(event.target.classList.contains("bar")){
+              dragMode = 1;
+              dragTag = event.target.parentNode;
+            }
+            if(dragTag){
+              dragStartX = event.clientX;
+              dragStartY = event.clientY;
+              dragStartTop = dragTag.offsetTop;
+              dragStartLeft = dragTag.offsetLeft;
+              dragStartWidth = dragTag.offsetWidth;
+              dragStartHeight = dragTag.offsetHeight;
+              osWidth = os.clientWidth;
+              osHeight = os.clientHeight;
+
+              switch(dragMode){
+                case 1:{
+                  dragXPaddingE = osWidth - dragStartWidth - osPadding;
+                  dragYPaddingS = osHeight - dragStartHeight - osPadding;
+                } break;
+                case 2:{
+                  dragDirectionN = false, dragDirectionS = false, dragDirectionW = false, dragDirectionE = false;
+                  switch(dragResizeDirection){
+                    case "nw": dragDirectionN = true; dragDirectionW = true; break;
+                    case "ne": dragDirectionN = true; dragDirectionE = true; break;
+                    case "sw": dragDirectionS = true; dragDirectionW = true; break;
+                    case "se": dragDirectionS = true; dragDirectionE = true; break;
+                    case "n": dragDirectionN = true; break;
+                    case "s": dragDirectionS = true; break;
+                    case "w": dragDirectionW = true; break;
+                    case "e": dragDirectionE = true; break;
+                  }
+                  dragXPaddingW = 0 - dragStartLeft + osPadding;
+                  dragXPaddingE = osWidth - (dragStartLeft + dragStartWidth) - osPadding;
+                  dragYPaddingN = 0 - dragStartTop + osPadding;
+                  dragYPaddingS = osHeight - (dragStartTop + dragStartHeight) - osPadding;
+                  dragXMin = dragStartWidth - windowSizeMin;
+                  dragYMin = dragStartHeight - windowSizeMin;
+                } break;
+              }
+            }
           }
+        });
+        os.addEventListener("mousemove",(event)=>{
           if(dragTag){
-            dragStartX = event.clientX;
-            dragStartY = event.clientY;
-            dragStartTop = dragTag.offsetTop;
-            dragStartLeft = dragTag.offsetLeft;
+            const dragXMove = event.clientX - dragStartX;
+            const dragYMove = event.clientY - dragStartY;
             switch(dragMode){
               case 1:{
-                xMax = dragContainer.clientWidth - dragTag.offsetWidth;
-                yMax = dragContainer.clientHeight - dragTag.offsetHeight;
+                let x = Math.max(osPadding, Math.min(dragXMove + dragStartLeft, dragXPaddingE));
+                let y = Math.max(osPadding, Math.min(dragYMove + dragStartTop, dragYPaddingS));
+                dragTag.style.left = x + "px";
+                dragTag.style.top = y + "px";
               } break;
               case 2:{
-                dragStartWidth = dragTag.offsetWidth;
-                dragStartHeight = dragTag.offsetHeight;
+                if(dragDirectionN){
+                  let y = (dragYMove < 0) ? (Math.max(dragYMove, dragYPaddingN)) : (Math.min(dragYMove, dragYMin));
+                  dragTag.style.height = dragStartHeight - y + "px";
+                  dragTag.style.top = dragStartTop + y + "px";
+                }else if(dragDirectionS){
+                  let y = Math.min(dragYMove, dragYPaddingS);
+                  dragTag.style.height = dragStartHeight + y + "px";
+                }
+                if(dragDirectionW){
+                  let x = (dragXMove < 0) ? (Math.max(dragXMove, dragXPaddingW)) : (Math.min(dragXMove, dragXMin));
+                  dragTag.style.width = dragStartWidth - x + "px";
+                  dragTag.style.left = dragStartLeft + x  + "px";
+                }else if(dragDirectionE){
+                  let x = Math.min(dragXMove, dragXPaddingE);
+                  dragTag.style.width = dragStartWidth + x + "px";
+                }
               } break;
             }
           }
-        }
-      });
-      dragContainer.addEventListener("mousemove",(event)=>{
-        if(dragTag){
-          const dragXMove = event.clientX - dragStartX;
-          const dragYMove = event.clientY - dragStartY;
-          switch(dragMode){
-            case 1:{
-              let x = Math.max(0, Math.min(dragXMove + dragStartLeft,xMax));
-              let y = Math.max(0, Math.min(dragYMove + dragStartTop,yMax));
-              dragTag.style.left = x + "px";
-              dragTag.style.top = y + "px";
-            } break;
-            case 2:{
-              let n = false, s = false, w = false, e = false;
-              switch(dragResizeDirection){
-                case "nw": n = true; w = true; break;
-                case "ne": n = true; e = true; break;
-                case "sw": s = true; w = true; break;
-                case "se": s = true; e = true; break;
-                case "n": n = true; break;
-                case "s": s = true; break;
-                case "w": w = true; break;
-                case "e": e = true; break;
-              }
-              if(n){
-                dragTag.style.height = dragStartHeight - dragYMove + "px";
-                dragTag.style.top = dragStartTop + dragYMove + "px";
-              }else if(s){
-                dragTag.style.height = dragStartHeight + dragYMove + "px";
-              }
-              if(w){
-                dragTag.style.width = dragStartWidth - dragXMove + "px";
-                dragTag.style.left = dragStartLeft + dragXMove  + "px";
-              }else if(e){
-                dragTag.style.width = dragStartWidth + dragXMove + "px";
-              }
-            } break;
-          }
-        }
-      });
-      dragContainer.addEventListener("mouseup",()=>{ dragTag = null; });
+        });
+        os.addEventListener("mouseup",()=>{ dragTag = null; });
+      }
     }
   }
 
