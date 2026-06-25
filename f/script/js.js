b143e272d17b2bb25bebb4cd202f15da035125af
peu0em <peu0em@gmail.com>
2026-06-25 09:37:23 +0900

    window

--- f/script/js.js
+++ f/script/js.js
@@ -4,17 +4,21 @@ windowtag.innerHTML = `
   <div class='content'></div>
 `
 const window_bar = document.createElement("div");
-window_bar.classList.add("bar");
+window_bar.classList.add("bar","grab");
 window_bar.innerHTML = `
   <img class='x' src='/f/visual/button_x.svg' alt='close' role='button'>
 `
 const window_resizer = document.createElement("div");
 window_resizer.classList.add("resizer");
 window_resizer.innerHTML = `
-  <div class='top'></div>
-  <div class='bottom'></div>
-  <div class='left'></div>
-  <div class='right'></div>
+  <div class='n grab'></div>
+  <div class='s grab'></div>
+  <div class='w grab'></div>
+  <div class='e grab'></div>
+  <div class='nw grab'></div>
+  <div class='ne grab'></div>
+  <div class='sw grab'></div>
+  <div class='se grab'></div>
 `
 
 
@@ -23,7 +27,6 @@ window_resizer.innerHTML = `
 
 
     const windowFirst = document.getElementsByClassName("window");
-    console.log(windowFirst);
     for(const tag of windowFirst){
       if(!tag.getElementsByClassName("bar")[0]){
         tag.insertBefore(window_bar,tag.firstChild); //.cloneNode(true)
@@ -36,25 +39,76 @@ window_resizer.innerHTML = `
     {
       const dragContainer = document.body;
       let dragTag;
-      let dragStartX, dragStartY;
+      let dragStartX, dragStartY, dragStartWidth, dragStartHeight, dragStartTop, dragStartLeft;
       let xMax, yMax;
+      let dragMode = 0; // 1 - move, 2 - resize
+      let dragResizeDirection;
       dragContainer.addEventListener("mousedown",(event)=>{
-        if(event.target.classList.contains("bar")){
-          dragTag = event.target.parentNode;
-        }
-        if(dragTag){
-          dragStartX = event.clientX - dragTag.offsetLeft;
-          dragStartY = event.clientY - dragTag.offsetTop;
-          xMax = dragContainer.clientWidth - dragTag.offsetWidth;
-          yMax = dragContainer.clientHeight - dragTag.offsetHeight;
+        if(event.target.classList.contains("grab")){
+          event.preventDefault();
+          if(event.target.parentNode.classList.contains("resizer")){
+            dragMode = 2;
+            dragTag = event.target.parentNode.parentNode;
+            dragResizeDirection = event.target.classList[0];
+          }else if(event.target.classList.contains("bar")){
+            dragMode = 1;
+            dragTag = event.target.parentNode;
+          }
+          if(dragTag){
+            dragStartX = event.clientX;
+            dragStartY = event.clientY;
+            dragStartTop = dragTag.offsetTop;
+            dragStartLeft = dragTag.offsetLeft;
+            switch(dragMode){
+              case 1:{
+                xMax = dragContainer.clientWidth - dragTag.offsetWidth;
+                yMax = dragContainer.clientHeight - dragTag.offsetHeight;
+              } break;
+              case 2:{
+                dragStartWidth = dragTag.offsetWidth;
+                dragStartHeight = dragTag.offsetHeight;
+              } break;
+            }
+          }
         }
       });
       dragContainer.addEventListener("mousemove",(event)=>{
         if(dragTag){
-          let x = Math.max(0, Math.min(event.clientX - dragStartX,xMax));
-          let y = Math.max(0, Math.min(event.clientY - dragStartY,yMax));
-          dragTag.style.left = x + "px";
-          dragTag.style.top = y + "px";
+          const dragXMove = event.clientX - dragStartX;
+          const dragYMove = event.clientY - dragStartY;
+          switch(dragMode){
+            case 1:{
+              let x = Math.max(0, Math.min(dragXMove + dragStartLeft,xMax));
+              let y = Math.max(0, Math.min(dragYMove + dragStartTop,yMax));
+              dragTag.style.left = x + "px";
+              dragTag.style.top = y + "px";
+            } break;
+            case 2:{
+              let n = false, s = false, w = false, e = false;
+              switch(dragResizeDirection){
+                case "nw": n = true; w = true; break;
+                case "ne": n = true; e = true; break;
+                case "sw": s = true; w = true; break;
+                case "se": s = true; e = true; break;
+                case "n": n = true; break;
+                case "s": s = true; break;
+                case "w": w = true; break;
+                case "e": e = true; break;
+              }
+              if(n){
+                dragTag.style.height = dragStartHeight - dragYMove + "px";
+                dragTag.style.top = dragStartTop + dragYMove + "px";
+              }else if(s){
+                dragTag.style.height = dragStartHeight + dragYMove + "px";
+              }
+              if(w){
+                dragTag.style.width = dragStartWidth - dragXMove + "px";
+                dragTag.style.left = dragStartLeft + dragXMove  + "px";
+              }else if(e){
+                dragTag.style.width = dragStartWidth + dragXMove + "px";
+              }
+            } break;
+          }
         }
       });
       dragContainer.addEventListener("mouseup",()=>{ dragTag = null; });
